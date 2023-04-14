3d501ae073acdce2e9fa3694fe63779fae2dabda
DarkkLegion <arshad18syed@gmail.com>
2023-04-14 15:28:20 +0530

    about section designed

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -22,7 +22,7 @@ const Computers = ({isMobile}) => {
       />
       <primitive 
         object={computer.scene} 
-        scale={isMobile ? 0.7 : 0.75} 
+        scale={isMobile ? 0.7 : 0.75}
         position={isMobile ? [0,-3,-2.2] : [0,-3.25,-1.5]}/>
 
     </mesh>
@@ -55,6 +55,8 @@ const ComputersCanvas = () => {
         enableZoom={false}
         maxPolarAngle={Math.PI / 2}
         minPolarAngle={Math.PI / 2}
+        autoRotate={true}
+        autoRotateSpeed={-0.8}
         />
         <Computers isMobile={isMobile}/>
       </Suspense>
