4943fa8ae799d549516d68bb60147dae8be73440
DarkkLegion <arshad18syed@gmail.com>
2023-04-15 21:55:28 +0530

    tech section designed with rotating balls

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"
+import { SectionWrapper } from "../hoc"
+import { technologies } from "../constants"
+import {styles} from '../styles'
+
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    <div className="flex flex-row flex-wrap justify-center gap-10 ">
+      {technologies.map((technology) => (
+        <div key={technology.name} className="w-28 h-28 text-center">
+          <BallCanvas icon={technology.icon} />
+          <p className="mt-2 font-medium text-[14px]">{technology.name}</p>
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "tech")
