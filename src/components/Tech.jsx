20a85a0cc2c9b56e1e9752f637b437d90085c2a7
DarkkLegion <arshad18syed@gmail.com>
2023-04-16 13:35:05 +0530

    minor improvements

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,19 +2,27 @@ import { BallCanvas } from "./canvas"
 import { SectionWrapper } from "../hoc"
 import { technologies } from "../constants"
 import {styles} from '../styles'
-
+import { motion } from 'framer-motion'
+import {fadeIn, textVariant} from '../utils/motion'
 
 
 const Tech = () => {
   return (
-    <div className="flex flex-row flex-wrap justify-center gap-10 ">
-      {technologies.map((technology) => (
-        <div key={technology.name} className="w-28 h-28 text-center">
-          <BallCanvas icon={technology.icon} />
-          <p className="mt-2 font-medium text-[14px]">{technology.name}</p>
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+          <p className={styles.sectionSubText}>My Technical Expertise</p>
+          <h2 className={styles.sectionHeadText}>Technologies.</h2>
+      </motion.div>
+      <div className="flex flex-row flex-wrap justify-center gap-10 ">
+        {technologies.map((technology) => (
+          <div key={technology.name} className="w-28 h-28 text-center">
+            <BallCanvas icon={technology.icon} />
+            <p className="mt-2 font-medium text-[14px]">{technology.name}</p>
+          </div>
+        ))}
+      </div>
+    </>
+    
   )
 }
 
