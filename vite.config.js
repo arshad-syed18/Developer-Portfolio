ffc31787bcea02495e630b616852bd3503e9b948
DarkkLegion <arshad18syed@gmail.com>
2023-04-18 08:15:37 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://www.syedarshad.tech',
+  base: 'https://www.syedarshad.tech',
   plugins: [react()],
 })
